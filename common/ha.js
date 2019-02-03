d2c40402f84eb882b62b8cd4addf4ad232f6de5d
Timmo <contact@timmo.xyz>
2019-02-03 14:01:04 +0000

    :sparkles: Add heating

--- common/ha.js
+++ common/ha.js
@@ -1,5 +1,8 @@
-let request = require('superagent'),
-  config = require('../config.json'),
+let WebSocket = require('ws'),
+  config = require('../config.json').hass,
+  ws = new WebSocket(config.url),
+  states = [],
+  ready = false,
   source = [
     'source-tv-virgintivo',
     'source-tv-androidtv',
@@ -7,34 +10,100 @@ let request = require('superagent'),
     'source-speaker-livingroom',
     'source-speaker-kitchen'
   ],
-  sourceId = 0;
+  sourceId = -1;
 
-const call = (domain, service, entity_id) => {
-  console.log(domain, service, entity_id);
-  request
-    .post(`${config.hass.url}/api/services/${domain}/${service}`)
-    .set('Authorization', `Bearer ${config.hass.token}`)
-    .send({ entity_id })
-    .then(res => console.log(res.body))
-    .catch(err => console.error(err));
+ws.on('open', open = () => {
+  console.log('WS: Opened');
+});
+
+ws.on('message', incoming = data => {
+  // console.log('WS: Message received:', data);
+  const message = JSON.parse(data);
+  if (message.type)
+    switch (message.type) {
+      default:
+        console.log('WS: Unhandled message type received:', data);
+        break;
+      case 'auth_required':
+        ws.send(JSON.stringify({
+          type: 'auth',
+          access_token: config.token
+        }), err => err ? console.error('WS:', err) : console.log('WS: Sent token'));
+        break;
+      case 'auth_ok':
+        ready = true;
+        ws.send(JSON.stringify({
+          id: 19,
+          type: 'get_states',
+        }), err => err ? console.error('WS:', err) : console.log('WS: Sent get states'));
+        break;
+      case 'result':
+        // console.log('WS: result:', message);
+        if (message.id === 19 && message.success && message.result) {
+          states = message.result;
+          console.log('WS: States set');
+          ws.send(JSON.stringify({
+            id: 18,
+            type: 'subscribe_events',
+            event_type: 'state_changed'
+          }), err => err ? console.error('WS:', err) : console.log('WS: Sent subscribe'));
+        }
+        break;
+      case 'event':
+        console.log('WS: event');
+        if (message.event.event_type)
+          switch (message.event.event_type) {
+            default:
+              console.log('WS: Unhandled event type message received:', data);
+            case 'state_changed':
+              console.log('WS: state_changed');
+              break;
+          }
+        else console.log('WS: Unhandled event message received:', data);
+        break;
+    }
+  else console.log('WS: Unhandled message received:', data);
+});
+
+const call = (domain, service, data) => {
+  console.log(domain, service, data);
+  if (ready)
+    ws.send(JSON.stringify({
+      id: 24,
+      type: 'call_service',
+      domain,
+      service,
+      service_data: data
+    }), err => err ? console.error('WS:', err) : console.log('WS: Sent call service'));
 };
 
-const nextSource = () => sourceId = sourceId === source.length - 1 ? 0 : sourceId + 1;
+const nextSource = () => {
+  sourceId = sourceId === source.length - 1 ? 0 : sourceId + 1;
+  return source[sourceId];
+};
 
 const getSourceName = () => {
   switch (sourceId) {
     case 0: return 'media_player.virgin_v6';
     case 1: return 'media_player.xiaomi_mi_box_s';
-    case 2: return 'media_player.living_room_tv_2';
+    case 2: return 'media_player.lg_tv_remote';
     case 3: return 'media_player.living_room_speaker';
     case 4: return 'media_player.kitchen_speaker';
   }
 };
 
+const getData = (entity_id, cb) => {
+  const item = {
+    data: states.find(s => s.entity_id === entity_id),
+    cb
+  };
+  states.push(item);
+  cb(item.data);
+};
+
 module.exports = {
   call,
-  source,
-  sourceId,
   nextSource,
-  getSourceName
+  getSourceName,
+  getData
 };
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let path = require('path'),
   StreamDeck = require('elgato-stream-deck'),
   cp = require('child_process'),
-  opn = require('opn'),
-  ks = require('node-key-sender'),
+  // opn = require('opn'),
+  // ks = require('node-key-sender'),
   ha = require('./common/ha'),
   image = require('./common/image'),
   moment = require('moment'),
@@ -13,7 +13,8 @@ let path = require('path'),
   BRIGHTNESS = 50,
   lastPage = PAGE_MAIN,
   currPage = PAGE_MAIN,
-  timeouts = [];
+  timeouts = [],
+  clockInterval;
 
 getResource = filePath => path.resolve(__dirname, `resources/${filePath}`);
 
@@ -40,8 +41,31 @@ updateBrightness = () => {
   ));
 };
 
+const runClock = () => {
+  if (clockInterval) clearTimeout(clockInterval);
+  clockInterval = setInterval(() => {
+    let blank = getResource('images/blank.png'),
+      time = moment();
+    image.addText(blank, time.format('HH'), true, filePath =>
+      setTempImageAndDelete(14, filePath)
+    );
+    image.addText(blank, time.format('mm'), true, filePath =>
+      setTempImageAndDelete(13, filePath)
+    );
+    image.addText(blank, time.format('dd'), true, filePath =>
+      setTempImageAndDelete(12, filePath)
+    );
+    image.addText(blank, time.format('DD'), true, filePath =>
+      setTempImageAndDelete(11, filePath)
+    );
+  }, 1000);
+};
+
 createPage = page => {
-  for (let i = 0; i <= 10; i++)streamDeck.clearKey(i);
+  let blank = getResource('images/blank.png'),
+    back = getResource('images/back.png');
+  clearInterval(clockInterval);
+  for (let i = 0; i <= 10; i++) { streamDeck.clearKey(i); }
   switch (page) {
     case PAGE_MAIN:
       lastPage = PAGE_MAIN;
@@ -58,32 +82,16 @@ createPage = page => {
       // streamDeck.fillImageFromFile(6, getResource('images/skip-next.png'));
       // streamDeck.fillImageFromFile(5, getResource('images/vol-up.png'));
       // Row 3
-      setInterval(() => {
-        let blank = getResource('images/blank.png'),
-          time = moment();
-
-        image.addText(blank, time.format('HH'), true, filePath =>
-          setTempImageAndDelete(14, filePath)
-        );
-        image.addText(blank, time.format('mm'), true, filePath =>
-          setTempImageAndDelete(13, filePath)
-        );
-        image.addText(blank, time.format('dd'), true, filePath =>
-          setTempImageAndDelete(12, filePath)
-        );
-        image.addText(blank, time.format('DD'), true, filePath =>
-          setTempImageAndDelete(11, filePath)
-        );
-      }, 1000);
+      runClock();
       streamDeck.fillImageFromFile(10, getResource('images/settings.png'));
       break;
     case PAGE_HA:
       lastPage = PAGE_MAIN;
       // Row 1
-      streamDeck.fillImageFromFile(4, getResource('images/back.png'));
+      streamDeck.fillImageFromFile(4, back);
       streamDeck.fillImageFromFile(3, getResource('images/ha/reset.png'));
       streamDeck.fillImageFromFile(2, getResource('images/ha/night.png'));
-      streamDeck.fillImageFromFile(1, getResource('images/ha/heating-boost.png'));
+      streamDeck.fillImageFromFile(1, getResource('images/ha/heating.png'));
       // streamDeck.fillImageFromFile(0, getResource('images/ha/source-tv.png'));
       // Row 2
       streamDeck.fillImageFromFile(9, getResource('images/ha/vol-down.png'));
@@ -92,21 +100,45 @@ createPage = page => {
       streamDeck.fillImageFromFile(6, getResource('images/ha/skip-next.png'));
       streamDeck.fillImageFromFile(5, getResource('images/ha/vol-up.png'));
       // Row 3
-      streamDeck.fillImageFromFile(10, getResource(`images/ha/${ha.source[ha.sourceId]}.png`));
+      runClock();
+      streamDeck.fillImageFromFile(10, getResource(`images/ha/${ha.nextSource()}.png`));
       break;
     case PAGE_HA_HEATING:
       lastPage = PAGE_HA;
       // Row 1
-      streamDeck.fillImageFromFile(4, getResource('images/back.png'));
-
-      streamDeck.fillImageFromFile(0, getResource('images/ha/heating-boost.png'));
+      streamDeck.fillImageFromFile(4, back);
+      streamDeck.fillImageFromFile(1, getResource('images/ha/heating/up.png'));
+      // Row 2
+      // console.log(ch);
+      ha.getData('climate.central_heating', data => {
+        image.addText(blank, data.attributes.current_temperature.toString(), true, filePath =>
+          setTempImageAndDelete(8, filePath)
+        );
+        image.addText(blank, data.attributes.temperature.toString(), true, filePath =>
+          setTempImageAndDelete(6, filePath)
+        );
+      });
+      // Row 3
+      streamDeck.fillImageFromFile(14, getResource('images/ha/heating/boost.png'));
+      setTimeout(() => {
+        streamDeck.fillImageFromFile(13, blank);
+        streamDeck.fillImageFromFile(12, blank);
+      }, 200);
+      streamDeck.fillImageFromFile(11, getResource('images/ha/heating/down.png'));
       break;
     case PAGE_SETTINGS:
       lastPage = PAGE_MAIN;
       // Row 1
-      streamDeck.fillImageFromFile(4, getResource('images/back.png'));
+      streamDeck.fillImageFromFile(4, back);
       streamDeck.fillImageFromFile(3, getResource('images/brightness-down.png'));
       streamDeck.fillImageFromFile(2, getResource('images/brightness-up.png'));
+      // Row 3
+      setTimeout(() => {
+        streamDeck.fillImageFromFile(14, blank);
+        streamDeck.fillImageFromFile(13, blank);
+        streamDeck.fillImageFromFile(12, blank);
+        streamDeck.fillImageFromFile(11, blank);
+      }, 200);
       break;
   }
   currPage = page;
@@ -128,7 +160,7 @@ handleKeyPressed = key => {
         // case PAGE_MAIN:
         //   return cp.exec('discord');
         case PAGE_HA:
-          return ha.call('scene', 'turn_on', 'scene.reset_lights');
+          return ha.call('scene', 'turn_on', { entity_id: 'scene.reset_lights' });
         case PAGE_SETTINGS:
           BRIGHTNESS -= 5;
           return updateBrightness();
@@ -138,7 +170,7 @@ handleKeyPressed = key => {
         // case PAGE_MAIN:
         //   return cp.exec('spotify');
         case PAGE_HA:
-          return ha.call('scene', 'turn_on', 'scene.night_mode');
+          return ha.call('scene', 'turn_on', { entity_id: 'scene.night_mode' });
         case PAGE_SETTINGS:
           BRIGHTNESS += 5;
           return updateBrightness();
@@ -149,63 +181,81 @@ handleKeyPressed = key => {
         //   return opn('https://github.com');
         case PAGE_HA:
           return createPage(PAGE_HA_HEATING);
+        case PAGE_HA_HEATING:
+          return ha.getData('climate.central_heating', data =>
+            ha.call('climate', 'set_temperature', {
+              entity_id: 'climate.central_heating',
+              temperature: data.attributes.temperature + 0.1
+            })
+          );
       }
     case 0:
       switch (currPage) {
         // case PAGE_MAIN:
         //   return opn('https://gitlab.com');
-        case PAGE_HA_HEATING:
-          return ha.call('scene', 'turn_on', 'scene.heating_boost');
       }
     case 9:
       switch (currPage) {
         // case PAGE_MAIN:
         //   return ks.sendKey('');
         case PAGE_HA:
-          return ha.call('media_player', 'volume_down', ha.getSourceName());
+          return ha.call('media_player', 'volume_down', { entity_id: ha.getSourceName() });
       }
     case 8:
       switch (currPage) {
         // case PAGE_MAIN:
         //   return ks.sendKey('');
         case PAGE_HA:
-          return ha.call('media_player', 'media_previous_track', ha.getSourceName());
+          return ha.call('media_player', 'media_previous_track', { entity_id: ha.getSourceName() });
       }
     case 7:
       switch (currPage) {
         // case PAGE_MAIN:
         //   return ks.sendKey('');
         case PAGE_HA:
-          return ha.call('media_player', 'media_play_pause', ha.getSourceName());
+          return ha.call('media_player', 'media_play_pause', { entity_id: ha.getSourceName() });
       }
     case 6:
       switch (currPage) {
         // case PAGE_MAIN:
         //   return ks.sendKey('');
         case PAGE_HA:
-          return ha.call('media_player', 'media_next_track', ha.getSourceName());
+          return ha.call('media_player', 'media_next_track', { entity_id: ha.getSourceName() });
       }
     case 5:
       switch (currPage) {
         // case PAGE_MAIN:
         //   return ks.sendKey('');
         case PAGE_HA:
-          return ha.call('media_player', 'volume_up', ha.getSourceName());
+          return ha.call('media_player', 'volume_up', { entity_id: ha.getSourceName() });
       }
     case 14:
+      switch (currPage) {
+        case PAGE_HA_HEATING:
+          return ha.call('scene', 'turn_on', { entity_id: 'scene.heating_boost' });
+      }
       return;
     case 13:
       return;
     case 12:
       return;
     case 11:
+      switch (currPage) {
+        case PAGE_HA_HEATING:
+          return ha.getData('climate.central_heating', data =>
+            ha.call('climate', 'set_temperature', {
+              entity_id: 'climate.central_heating',
+              temperature: data.attributes.temperature - 0.1
+            })
+          );
+      }
       return;
     case 10:
       switch (currPage) {
         case PAGE_MAIN:
           return createPage(PAGE_SETTINGS);
         case PAGE_HA:
-          return streamDeck.fillImageFromFile(10, getResource(`images/ha/${ha.source[ha.nextSource()]}.png`));
+          return streamDeck.fillImageFromFile(10, getResource(`images/ha/${ha.nextSource()}.png`));
       }
   }
 };
